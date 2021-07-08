32d3df1c306c6321f98a53b0fa6df65aeb3731d1
Juan Carlos Zenteno <juancarlos_zp@ciencias.unam.mx>
2021-07-07 21:09:07 -0500

    Fixed some minor mistakes

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0/9.0;
     let image_width: u32 = 400;
     let image_height: u32 = (image_width as f64 / ASPECT_RATIO) as u32;
-    let samples_per_pixel: u8 = 100;
+    let samples_per_pixel: u8 = 75;
 
     //World
     let mut world = HittableList::new();
@@ -47,12 +47,12 @@ fn main() {
     for j in (0..image_height).rev() {
         eprint!("\x1B[2J");
         eprintln!("Scanlines remaining: {}", j);
-        for _ in 0..image_width {
+        for i in 0..image_width {
             let mut pixel_color: Color = Color::new(0.0, 0.0, 0.0);
-            for i in 0..samples_per_pixel {
+            for _ in 0..samples_per_pixel {
                 let mut rng = rand::thread_rng();
-                let u: f64 = i as f64 + rng.gen_range(0.0..1.0)/ (image_width - 1) as f64;
-                let v: f64 = j as f64 + rng.gen_range(0.0..1.0)/ (image_height - 1) as f64;
+                let u: f64 = (i as f64 + rng.gen_range(0.0..0.9))/ (image_width - 1) as f64;
+                let v: f64 = (j as f64 + rng.gen_range(0.0..0.9))/ (image_height - 1) as f64;
 
                 let ray = camera.get_ray(u, v);
                 pixel_color = pixel_color + ray_color(&ray, &world);
